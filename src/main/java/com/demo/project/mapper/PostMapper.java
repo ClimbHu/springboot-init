56501996c33447fd6aefccbcaad49a99637860d0
ClimbHu <proberj@163.com>
2024-03-11 22:47:58 +0800

    springboot 模板

---
+++ src/main/java/com/demo/project/mapper/PostMapper.java
@@ -0,0 +1,17 @@
+package com.demo.project.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.demo.project.model.entity.Post;
+
+/**
+* @description 针对表【post(帖子)】的数据库操作Mapper
+* @createDate 2022-09-13 16:03:41
+* @Entity com.demo.project.model.entity.Post
+*/
+public interface PostMapper extends BaseMapper<Post> {
+
+}
+
+
+
+
---
+++ src/main/java/com/demo/project/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.demo.project.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.demo.project.model.entity.User;
+
+/**
+ * @Entity com.demo.project.model.domain.User
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
